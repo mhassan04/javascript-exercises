54a841bff8561280c0ad500108ecf3874f35b690
Mahamed Hassan <84216239+mhassan04@users.noreply.github.com>
2024-11-06 22:37:49 -0500

    finished 08 exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current, 1);
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return Math.pow(x,y);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num == 0){
+    return 1;
+  }
+  let product = 1;
+  for(let i = num; i > 0; i--){
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
