3a35ee794d42ddd60d79ffb04339520c52f44e60
Mahamed Hassan <84216239+mhassan04@users.noreply.github.com>
2024-11-07 19:32:40 -0500

    completed exercise 09
    wrote a function that determines whether or not a given string is a palindrome.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (word) {
+ const alphabetsandnums = 'qwertyuiopasdfghjklzxcvbnm1234567890';
 
+ const newString = word.toLowerCase().split('').filter((character) => alphabetsandnums.includes(character)).join('');
+
+ const reversedString = newString.split('').reverse().join('');
+
+ return newString === reversedString;
 };
 
 // Do not edit below this line
